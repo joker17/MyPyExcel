f5bb8da4832315324411807aa1c4f0b8b872ff79
unknown <hejiang18696@hs.handsome.com.cn>
2018-01-09 17:43:38 +0800

    ci

--- readxls.py
+++ readxls.py
@@ -2,6 +2,8 @@
 #import pyexcel_io
 import pyexcel as pe
 
-records = pe.iget_records(file_name="./data1.xls")
+#records = pe.iget_records(file_name="./data1.xls")
+records = pe.iget_records(file_name="./test_xlsx.xlsx")
+print("[%s]", records[0][1])
 for record in records:
     print("%s is aged at %d" % (record['NAME'], record['AGE']))
